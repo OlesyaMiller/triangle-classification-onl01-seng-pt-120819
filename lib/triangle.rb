f69562c79dc98c6d2477c8233d38316a7ac4f121
Olesya Miller <olesyam.miller@gmail.com>
2020-01-07 00:35:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind 
     if self.side1 <= 0 || self.side2 <= 0 || self.side3 <= 0
         raise TriangleError
-      elsif self.side1 + self.
+      elsif (self.side1 + self.side2) <= self.side3 || (self.side1 + self.side3) <= self.side2 || (self.side2 + self.side3) <= self.side1
         raise TriangleError
       elsif self.side1 == self.side2 && self.side2 == self.side3 
         :equilateral
