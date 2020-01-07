396b818201b7b3fcdffee855d838a771097744a4
Olesya Miller <olesyam.miller@gmail.com>
2020-01-07 00:00:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,14 @@ class Triangle
 
   attr_accessor :side1, :side2, :side3
 
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  # def initialize(side1, side2, side3)
+  #   @side1 = side1
+  #   @side2 = side2
+  #   @side3 = side3
+  # end
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
   
   def kind 
