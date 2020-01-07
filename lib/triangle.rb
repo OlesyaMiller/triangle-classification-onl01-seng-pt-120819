0a960e4f6c3532aca9839a72915f1d16c44332ba
Olesya Miller <olesyam.miller@gmail.com>
2020-01-07 00:09:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,21 +9,23 @@ class Triangle
   end
   
   def kind 
-    if self.side1 == self.side2 && self.side2 == self.side3 
-      :equilateral
+    if self.side1 < 0 || self.side1 == 0 
+      
+      elsif self.side1 == self.side2 && self.side2 == self.side3 
+        :equilateral
       elsif self.side2 == self.side3 || self.side1 == self.side3 || self.side1 == self.side2
         :isosceles
       else
         :scalene 
     end
     
-    #if self.side1 < 0 || self.side1 == 0 
+    if self.side1 < 0 || self.side1 == 0 
       
   end
   
   class TriangleError 
     def message
-      "you must give the get_married method an argument of an instance of the person class!"
+      "ERROR"
     end
   end
 end
